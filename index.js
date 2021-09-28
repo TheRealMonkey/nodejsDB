7a90a44044b07da840b1d8384b7d302c7aa76ad8
TheRealMonkey <mariskalnins@protonmail.com>
2021-09-28 12:01:56 +0000

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var sqlite3 = require("sqlite3");
+var app = express();
+app.use(express.static(__dirname + "/client"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.locals.db = new sqlite3.Database(
+  path.resolve(__dirname, "database/form.db")
+);
+app.locals.db.run(
+  "CREATE TABLE IF NOT EXISTS form( fID TEXT, fCode TEXT, fA TEXT, fA0 TEXT, fA1 TEXT, fA2 TEXT, fA3 TEXT, fB TEXT, fB0 TEXT, fB1 TEXT, fB2 TEXT, fB3 TEXT, fC TEXT, fC0 TEXT, fC1 TEXT, fC2 TEXT, fC3 TEXT)"
+);
+
+app.post("/api", function (req, res) {
+  console.log(req.body.name, req.body.email, req.body.message);
+  res.send("hey");
+});
+
+app.use("/send", require("./routes/send"));
+
+app.get("*", function (req, res) {
+  res.redirect("/");
+});
+
+const port = 3000;
+app.listen(port);
+console.log(`Server started at ${port}`);
---
+++ routes/send.js
@@ -0,0 +1,23 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const router = express.Router();
+
+router.post("/", function (req, res, next) {
+    const db = req.app.locals.db;
+
+    console.log(req.body)
+    // db.serialize(() => {
+    //     db.run(
+    //         "INSERT INTO form(fID,  fCode,  fA,  fA0,  fA1,  fA2,  fA3,  fB,  fB0,  fB1,  fB2,  fB3,  fC,  fC0,  fC1,  fC2,  fC3) VALUES(?,?,?)",
+    //         [ req.body.fID,  req.body.fCode,  req.body.fA,  req.body.fA0,  req.body.fA1,  req.body.fA2,  req.body.fA3,  req.body.fB,  req.body.fB0,  req.body.fB1,  req.body.fB2,  req.body.fB3,  req.body.fC,  req.body.fC0,  req.body.fC1,  req.body.fC2,  req.body.fC3],
+    //         function (err) {
+    //             if (err) {
+    //                 return console.log(err.message);
+    //             }
+    //             console.log("new form added");
+    //         })
+    // })
+})
+
+module.exports = router;
+
