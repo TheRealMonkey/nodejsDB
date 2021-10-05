5ce0e9c8385253f375e97288cdc72ed51b2702da
TheRealMonkey <mariskalnins@protonmail.com>
2021-10-05 09:16:01 +0000

    added WHERE

--- routes/show.js
+++ routes/show.js
@@ -4,15 +4,19 @@ const router = express.Router();
 router.get("/", function (req, res, next) {
     console.log("hey")
     const db = req.app.locals.db;
+    const query = `SELECT rowid,fID,  fCode,  fA,  fA0,  fA1,  fA2,  fA3,  fB,  fB0,  fB1,  fB2,  fB3,  fC,  fC0,  fC1,  fC2,  fC3 FROM form WHERE fID = ${req.query.code}`
     db.serialize(() => {
         db.all(
-            "SELECT rowid,fID,  fCode,  fA,  fA0,  fA1,  fA2,  fA3,  fB,  fB0,  fB1,  fB2,  fB3,  fC,  fC0,  fC1,  fC2,  fC3 FROM form",
+            query,
             (error, rows) => {
+                if (rows){
                 temp = [];
                 rows.forEach((item) => { temp.push(item)})
-                res.json(temp)
+                res.json(temp)}
+                else{res.send("nothing found with that code")}
             })
     })
-})
+}
+)
 
 module.exports = router;
